963d4a1bbdab5af260a59ea9180902a6de74ae7d
King Leonidas <72262071+L4bb3rs@users.noreply.github.com>
2023-04-20 12:28:51 +0100

    Change of structure
    Added and changed version elements to improve speed and simplicity. Currently needing further development to include other features.

--- didpix.py
+++ didpix.py
@@ -1,199 +1,127 @@
-from utils import ProgressBar, System, Log, Color
-from query_user import QueryUser
-from os import mkdir, path
-
-from dataclasses import dataclass
-from typing import List
+# from setuptools import setup
+# # setup(name='didpix',
+# #       version='0.1',
+# #       description='A tool for NFTs on Chia',
+# #       url='http://github.com/',
+# #       author='A group of mad men',
+# #       author_email='Add here',
+# #       license='MIT',
+# #       packages=['requests'],
+# #       install_requires=[
+# #           'markdown',
+# #       ],
+# #       zip_safe=False)
 
 import requests
-import csv
-import time
+from utils import System, Formatter, Color
+from query_user import QueryUser
+import pandas as pd
+import numpy as np
 
-
-## The classes which will store the downloaded data
-@dataclass
-class Owner:
-    name: str
-    did: str
-    address: str
-
-@dataclass
-class NFT:
-    name: str
-    id: str
-    owner: Owner
-    
 
 def display_title():
-    print("\t ___   _  ___       ___  _ __  _ \n" +
-      "\t| . \ | || . \     | . \| |\ \/  \n" +
-      "\t| |  || || |  | 📷 |  _/| | \ \  \n" + 
-      "\t|___/ |_||___/     |_|  |_|_/\_\ ")
+    print(
+        "\n" +
+        "\t ___   _  ___       ___  _ __  _ \n" +
+        "\t| . \\ | || . \\     | . \\| |\\ \\// \n" +
+        "\t| |  || || |  | 📷 |  _/| |/\\ \\  \n" +
+        "\t|___/ |_||___/     |_|  |_|_/\\_\\ \n")
 
 
-def mint_garden_get_request(url_request : str, allow_timeout : bool = False) -> dict:
+def fetch_collection_name(collection_id: str) -> str:
     try:
-        resp = requests.get(url=url_request, timeout=5.0)
-        resp.raise_for_status()  # Raise an exception if the request was unsuccessful
-        data = resp.json()
-        
-        return data
-        
-    except requests.exceptions.ConnectionError as e:
-        Log.ERR("Could not connect to MintGarden API. Please check your internet connection.")
-        System.exit("Please fix your internet connection, and try again.")
-        
-    except requests.exceptions.Timeout as e:
-        Log.ERR("The request to MintGarden API timed out, the server may be down or extremely busy.")
-        if not allow_timeout:
-            System.exit("Please try again later.")
-        
+        response = requests.get(
+            f"https://api.mintgarden.io/collections/{collection_id}")
+        # Raise an HTTPError if the status code is >= 400.
+        response.raise_for_status()
+        page_data = response.json()
+        return page_data['name']
     except requests.exceptions.RequestException as e:
-        Log.ERR(f"Error occurred while making the request: {e}")
-        
-    except ValueError as e:
-        Log.ERR(f"Error occurred while parsing the response: {e}")
+        #print(f"An error occurred while making a request to the API: {e}")
+        return ""
 
 
-def fetch_collection_name(collection_id : str) -> str:
-    data = mint_garden_get_request(f'https://api.mintgarden.io/collections/{collection_id}')
-    
-    if data and 'name' in data.keys():
-        return data['name']
-    
-    return ""
-    
+def fetch_data(collection_id: str, require_owners: bool) -> dict:
+    """
+    Fetch data from the API and return it as a dictionary.
+    """
+    data = {"items": []}
+    params = {
+        "require_owner": str(require_owners).lower(),
+        "require_price": "false",
+        "size": "100"}
 
-## Get all potentially NFT IDs from given collections
-def fetch_NFT_IDs(collections : List[str]) -> List[NFT]:
-    Log.INFO("Fetching all NFT IDs for all Collections")
-    nfts = []
-    
-    collection_index = 1
-    for col in collections:
-        print()
-        Log.INFO(f"{collection_index}/{len(collections)} Fetching NFT IDs for: {col['name']}")
-
-        data = mint_garden_get_request(f'https://api.mintgarden.io/collections/{col["ID"]}/nfts/ids')
-
-        for d in data:
-            nfts.append(NFT(d['name'], d['encoded_id'], None))
-
-        log_msg = f"{len(data)} NFTs located for {col['name']}"
-        if len(data) > 0:
-            Log.PASS(log_msg)
-        else:
-            Log.WARN(log_msg)
-            
-        collection_index += 1
-
-    print()
-    log_msg = f"{len(nfts)} NFTs located in total for all collections\n"
-    if len(nfts) > 0:
-        Log.PASS(log_msg)
-    else:
-        Log.WARN(log_msg)
-
-    return nfts
-
-
-# Define a sorting key function that extracts the owner name from NFT data
-def get_owner_did(nft : NFT) -> str:
-    return nft.owner.did
-
-
-def generate_results(nfts : List[NFT], excluded_DIDs : List[dict]):
-    Log.INFO("Creating and Opening the output files")
-    # Create the Output files as per the README.md
-    nft_ownership_file = open("./Output/nft_ownership.csv", 'w', encoding="utf-8", newline='')
-    nft_ownership_writer = csv.writer(nft_ownership_file, delimiter =',')
-
-    nft_ownership_agg_file = open("./Output/nft_ownership_agg.csv", 'w', encoding="utf-8", newline='')
-    nft_ownership_agg_writer = csv.writer(nft_ownership_agg_file, delimiter =',')
-
-    DIDless_file = open("./Output/DIDless.csv", 'w', encoding="utf-8", newline='')
-    DIDless_writer = csv.writer(DIDless_file, delimiter =',')
-
-    excluded_NFTs_file = open("./Output/excluded_NFTs.csv", 'w', encoding="utf-8", newline='')
-    excluded_NFTs_writer = csv.writer(excluded_NFTs_file, delimiter =',')
-
-    # Write the csv headers
-    nft_ownership_writer.writerow(['nft_name','nft_id','owner_name','owner_DID','owner_address'])
-    nft_ownership_agg_writer.writerow(['owner_name','NFTs_owned_count','owner_DID','owner_address'])
-    DIDless_writer.writerow(['nft_name','nft_id','owner_address'])
-    excluded_NFTs_writer.writerow(['nft_name','nft_id','owner_name','owner_DID','owner_address'])
-
-
-    ## Get Owner addresses for each NFT
-    did_assigned_nfts = []
-    for nft in ProgressBar.create(nfts, prefix = 'Fetching Wallet Addresses:', length = 25):
+    while True:
         try:
-            data = mint_garden_get_request(url_request=f'https://api.mintgarden.io/nfts/{nft.id}', allow_timeout=True)
-        except:
-            # If request has failed or timed out, try again after wait.
-            Log.NOTE(f'\nFetching: {nft.id} failed. Trying again in 5 seconds', end='')
-            for s in range(5):
-                time.sleep(s)
-                print(".", end="")
-            data = mint_garden_get_request(url_request=f'https://api.mintgarden.io/nfts/{nft.id}', allow_timeout=False)
+            response = requests.get(
+                f"https://api.mintgarden.io/collections/{collection_id}/nfts",
+                params=params)
+            # Raise an HTTPError if the status code is >= 400.
+            response.raise_for_status()
+            page_data = response.json()
+            if not page_data["items"]:  # Check if the page has no items
+                break
+            data["items"].extend(page_data["items"])
+            if "next" not in page_data:
+                break
+            params["page"] = page_data["next"]
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while making a request to the API: {e}")
+            break
+    return data
 
-        xch_address = data["owner_address"]["encoded_id"]
 
-        if data["owner"]:
-            nft.owner = Owner(data["owner"]["name"], data["owner"]["encoded_id"], xch_address)
-            if not nft.owner.name:
-                nft.owner.name = "Unclaimed DID"
-        else:
-            # The NFT is not assigned to a DID
-            DIDless_writer.writerow([nft.name, nft.id, xch_address])
-            continue
-        
-        rejected = False
-        for excluded_DID in excluded_DIDs:
-            if nft.owner.did == excluded_DID['ID']:
-                excluded_NFTs_writer.writerow([nft.name, nft.id, nft.owner.name, nft.owner.did, nft.owner.address])
-                excluded_DID['total_excluded'] += 1
-                rejected = True
+def create_csv_files(collection_id: str, require_owners: bool):
+    """
+    Fetch data from the API, extract required fields, and create three CSV files.
+    """
+    # Fetch data from the API
+    data = fetch_data(collection_id, require_owners)
 
-        if not rejected:
-            did_assigned_nfts.append(nft)
-            
-    # Sort the list of NFTs by owner name
-    sorted_nfts = sorted(did_assigned_nfts, key=get_owner_did)
+    # Extract the required fields from the API response
+    nft_ownership_data = [{'encoded_id': item['encoded_id'],
+                           'name': item['name'],
+                           'owner_name': item['owner_name'],
+                           'owner_encoded_id': item['owner_encoded_id'],
+                           'owner_address_encoded_id': item['owner_address_encoded_id']} for item in data['items']]
 
-    # Write the sorted list of NFTs
-    for nft in ProgressBar.create(sorted_nfts, prefix = 'Writing sorted list of Owned NFTs:', length = 17):
-        nft_ownership_writer.writerow([nft.name, nft.id, nft.owner.name, nft.owner.did, nft.owner.address])
-    
-    owner_names = []
-    counts = []
-    owner_DIDs = []
-    owner_addresses = []
-    
-    # Aggregate the List
-    for nft in ProgressBar.create(sorted_nfts, prefix = 'Writing aggregated list of Owners:', length = 17):
-        if nft.owner.did in owner_DIDs:
-            counts[owner_DIDs.index(nft.owner.did)] += 1
-        else:
-            owner_names.append(nft.owner.name)
-            counts.append(1)
-            owner_DIDs.append(nft.owner.did)
-            owner_addresses.append(nft.owner.address)
-    
-    # Write the aggregated list
-    for i in range(len(owner_names)):
-        nft_ownership_agg_writer.writerow([owner_names[i], counts[i], owner_DIDs[i], owner_addresses[i]])
+    nft_ownership_agg_data = [{'owner_name': item['owner_name'],
+                               'owner_encoded_id': item['owner_encoded_id'],
+                               'count_owner_encoded_id': item['owner_encoded_id'],
+                               'owner_address_encoded_id': item['owner_address_encoded_id']} for item in data['items']]
 
-    Log.PASS(f"Total of {len(sorted_nfts)} verifiably owned NFTs found")
+    didless_data = [{'name': item['name'],
+                     'encoded_id': item['encoded_id'],
+                     'owner_address_encoded_id': item['owner_address_encoded_id']} for item in data['items'] if not item['owner_encoded_id']]
 
-    # Close files
-    nft_ownership_file.close()
-    nft_ownership_agg_file.close()
-    DIDless_file.close()
-    excluded_NFTs_file.close()
-    
-    print()
-    Log.PASS("The DID Snapshot is complete!")
+    # TODO
+    excluded_nfts_data = []
+
+    # Create DataFrame from extracted data
+    nft_ownership_df = pd.DataFrame(nft_ownership_data)
+    nft_ownership_agg_df = pd.DataFrame(nft_ownership_agg_data)
+    didless_df = pd.DataFrame(didless_data)
+    excluded_nfts_df = pd.DataFrame(excluded_nfts_data)
+
+    # # Group by owner_name and calculate the counts for nft_ownership_agg.csv then create nft_ownership.csv
+    nft_ownership_agg_df["count_owner_encoded_id"] = nft_ownership_agg_df.groupby(
+        'owner_encoded_id')['owner_encoded_id'].transform('count')
+    nft_ownership_agg_df = nft_ownership_agg_df.drop_duplicates(
+        subset=['owner_encoded_id'])
+    # Replace empty cells with NaN
+    nft_ownership_agg_df.replace('', np.nan, inplace=True)
+    nft_ownership_agg_df = nft_ownership_agg_df.dropna(
+        subset=['owner_encoded_id'])
+    nft_ownership_df.to_csv('Output/nft_ownership.csv', index=False)
+
+    # Create nft_ownership_agg.csv
+    nft_ownership_agg_df.to_csv('Output/nft_ownership_agg.csv', index=False)
+
+    # Create DIDless.csv
+    didless_df.to_csv('Output/DIDless.csv', index=False)
+
+    # Create excluded_nfts.csv
+    excluded_nfts_df.to_csv('Output/excluded_NFTs.csv', index=False)
 
 
 def say_goodbye():
@@ -203,93 +131,86 @@ def say_goodbye():
     input(f"\n {Color.prompt()} Press Enter to exit application..")
 
 
-def is_ID_duplicate(new_id : str, list : List[dict]) -> bool:
-    for l in list:
-        if l["ID"] == new_id:
-            print()
-            Log.NOTE("This ID has already been added to the list.")
-            return True
-    return False
+def get_collection_info(collection_id_list):
+    # Page 1: Collection Information
+    Formatter.info("Page 1/3: Collection Information")
+
+    #print(f"\n{Color.white_highlight('Page 1/3: Collection Information')}")
+
+    while True:
+        collection_id = QueryUser.get_collection_id(
+            "What is the ID of the collection you would like to take a snapshot for?")
+
+        if fetch_collection_name(collection_id):
+            collection_id_list.append(collection_id)
+            break
+        else:
+            print("\nCollection ID not found, please check and re-enter")
+
+    # Add more collections if needed
+    while QueryUser.get_bool(
+            "Do you have another collection to add to the list?"):
+        collection_id = QueryUser.get_collection_id(
+            "What is the collection ID?")
+        if collection_id in collection_id_list:
+            print("Collection ID already added.")
+        elif fetch_collection_name(collection_id):
+            collection_id_list.append(collection_id)
+        else:
+            print("\nCollection ID not found, please check and re-enter")
 
 
-def verify_collection_ID(collection_id: str, collections : List[dict]):
-    print()
-    Log.INFO(f"Verifying that {collection_id} exists.")
-    name = fetch_collection_name(collection_id)
-    
-    if name == "":
-        Log.ERR(f"Collection with ID of {collection_id} does not exist on MintGarden")
-    else:
-        collections.append({"ID":collection_id, "name":name})
-    Log.PASS(f"{name} has been verified to exist, and added to the list.")
+def get_did_exculsions(excluded_dids):
+    # Page 2: DID Exclusion
+    if QueryUser.get_bool("Is there any DIDs that you would like to exclude?"):
+        excluded_dids.append({"ID": QueryUser.get_did(
+            "What is the first DID?"), "name": ""})
+        while QueryUser.get_bool(
+                "Do you have another DID to add to the list?"):
+            new_did = QueryUser.get_did("What is the DID?")
+            if new_did in excluded_dids:
+                print("You already entered this DID")
+            else:
+                excluded_dids.append(new_did)
 
+
+def did_snapshot(collection_id_list):
+    print(f"\n{Color.white_highlight(' Page 3/3 : DID Snapshot ')}\n")
+    Formatter.info("Creating and Opening the output files")
+    for collection in collection_id_list:
+        create_csv_files(collection, False)
+        print(f"{Color.green_highlight('COLLECTION ')}{collection}")
+        Formatter.success("The DID Snapshot is complete!")
+        print("\n")
+
+    input(f"\n {Color.prompt()} Press Enter to continue..")
 
 
 def main():
-    System.clear()
-    display_title()
-    
-    print(f"\n{Color.white_highlight(' Page 1/3 : Collection Information')}")
-    
-    collections = []
-    collection_id = QueryUser.get_collection_id("What is the ID of the collection you would like to take a snapshot for?")
-    verify_collection_ID(collection_id, collections)
-    while (QueryUser.get_bool("Do you have another collection to add to the list?")):
-        
-        collection_id = QueryUser.get_collection_id("What is the collection ID?")
-        if not is_ID_duplicate(collection_id, collections):
-            verify_collection_ID(collection_id, collections)
-    
+    # Call necessary functions to clear screen and display title
     System.clear()
     display_title()
 
-    print(f"\n{Color.white_highlight(' Page 2/3 : DID Exclusion')}")
+    collection_id_list = []
+    excluded_dids = []
 
-    # Get the DIDs to exclude from the snapshot
-    excluded_DIDs = []
-    if (QueryUser.get_bool("Is there any DIDs that you would like to exclude?")):
-        excluded_DIDs.append({"ID":QueryUser.get_DID("What is the first DID?"), "name":""})
-        excluded_DIDs[len(excluded_DIDs) - 1]['name'] = input(f"\n  {Color.prompt()} Enter a name for this DID: ")
-        
-        while (QueryUser.get_bool("Do you have another DID to add to the list?")):
-            new_DID = QueryUser.get_DID("What is the DID?")
-            
-            if not is_ID_duplicate(new_DID, excluded_DIDs):
-                name = input(f"\n  {Color.prompt()} Enter a name for this DID: ")
-                if name == "": name == "Unnamed Excluded DID"
-                
-                excluded_DIDs.append({"ID":new_DID, "name": name})
-    
-    # Add a counter to each excluded DID
-    for ed in excluded_DIDs:
-        ed['total_excluded'] = 0
-    
+    get_collection_info(collection_id_list=collection_id_list)
+    get_did_exculsions(excluded_dids=excluded_dids)
+
+    # Call necessary functions to clear screen and display title
     System.clear()
     display_title()
-    
-    print(f"\n{Color.white_highlight(' Page 3/3 : DID Snapshot ')}\n")
-    
-    nfts = fetch_NFT_IDs(collections)
-    
-    if not path.isdir("./Output"):
-        mkdir(dir)
-    
-    if len(nfts) > 0:
-        generate_results(nfts, excluded_DIDs)
-    elif len(collections) > 1 :
-        Log.ERR("No NFTs were located for any of the provided collections.")
-        System.exit("Please ensure that the collection IDs are correct, and that they contain NFTs.")
-    else:
-        Log.ERR("No NFTs were located for", collections[0]['name'])
-        System.exit("Please ensure that the collection ID is correct, and that it contains NFTs.")
-        
-    input(f"\n {Color.prompt()} Press Enter to continue..")
-        
+
+    # Perform DID snapshot for the collection ID list
+    did_snapshot(collection_id_list)
+
+    # Call necessary functions to clear screen and display title
     System.clear()
     display_title()
-    
+
+    # Say goodbye
     say_goodbye()
-        
-        
+
+
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,60 +1,76 @@
-from sys import exit
 import os
 
-class ProgressBar():
-    ## Helper function for generating progress bars    
-    def create(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#', printEnd = "\r"):
-        total = len(iterable)
-        
-        # Progress Bar Printing Function
-        def printProgressBar (iteration):
-            percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-            filledLength = int(length * iteration // total)
-            bar = fill * filledLength + '-' * (length - filledLength)
-            print(f'\r{Color.white_highlight("INFO   ")}{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-            
-        # Initial Call
-        printProgressBar(0)
-        
-        # Update Progress Bar
-        for i, item in enumerate(iterable):
-            yield item
-            printProgressBar(i + 1)
-        
-        # Print New Line on Complete
-        print()
-    
-    
-class Color():
-    def red_text(msg : str): return f"\033[1;31;40m{msg}\033[0m"
-    def green_text(msg : str): return f"\033[1;32;40m{msg}\033[0m"
-    def blue_text(msg : str): return f"\033[1;34;40m{msg}\033[0m"
-    def orange_text(msg : str): return f"\033[1;33;40m{msg}\033[0m"
-    
-    def white_highlight(msg : str):return f"\033[1;30;47m{msg}\033[0m "
-    
-    def yellow_highlight(msg : str): return f"\033[1;30;43m{msg}\033[0m "
-    def green_highlight(msg : str): return f"\033[1;37;42m{msg}\033[0m "
-    def red_highlight(msg : str): return f"\033[1;30;41m{msg}\033[0m "
-    
-    def prompt(): return f"\033[1;30;47m>>\033[0m "
-    
 
-class Log():
-    def INFO(msg : str): print(Color.white_highlight("INFO   ") + msg)
-    def WARN(msg : str): print(Color.yellow_highlight("WARNING") + msg)
-    def NOTE(msg : str): print(Color.yellow_highlight("NOTICE ") + msg)
-    def PASS(msg : str): print(Color.green_highlight("SUCCESS") + msg)
-    def ERR(msg : str): print(Color.red_highlight("ERROR  ") + msg)
-        
-        
+class Color():
+    """Helper class for defining colored text"""
+
+    @staticmethod
+    def red_text(text):
+        return f"\033[1;31;40m{text}\033[0m"
+
+    @staticmethod
+    def green_text(text):
+        return f"\033[1;32;40m{text}\033[0m"
+
+    @staticmethod
+    def blue_text(text):
+        return f"\033[1;34;40m{text}\033[0m"
+
+    @staticmethod
+    def orange_text(text):
+        return f"\033[1;33;40m{text}\033[0m"
+
+    @staticmethod
+    def white_highlight(text):
+        return f"\033[1;30;47m{text}\033[0m "
+
+    @staticmethod
+    def yellow_highlight(text):
+        return f"\033[1;30;43m{text}\033[0m "
+
+    @staticmethod
+    def green_highlight(text):
+        return f"\033[1;37;42m{text}\033[0m "
+
+    @staticmethod
+    def red_highlight(text):
+        return f"\033[1;30;41m{text}\033[0m "
+
+    @staticmethod
+    def prompt():
+        return f"\033[1;30;47m>>\033[0m "
+
+
+class Formatter():
+    """Helper class for formatting messages"""
+
+    @staticmethod
+    def info(message):
+        print(Color.white_highlight("INFO       ") + message)
+
+    @staticmethod
+    def warning(message):
+        print(Color.yellow_highlight("WARNING") + message)
+
+    @staticmethod
+    def note(message):
+        print(Color.yellow_highlight("NOTICE ") + message)
+
+    @staticmethod
+    def success(message):
+        print(Color.green_highlight("SUCCESS    ") + message)
+
+    @staticmethod
+    def error(message):
+        print(Color.red_highlight("ERROR  ") + message)
+
+
 class System():
+    """Helper class for system-related tasks"""
+
+    @staticmethod
     def clear():
-        if os.name == 'nt': os.system('cls') # Clear command for Windows
-        else: os.system('clear') # Clear command for Unix/Linux/MacOS
-    
-    def exit(msg : str = "Elected to Quit Application"):
-        print("\n")
-        Log.NOTE(f"{msg}\n\n {Color.prompt()} Press Enter to exit..")
-        input()
-        exit()
+        if os.name == 'nt':
+            os.system('cls')  # Clear command for Windows
+        else:
+            os.system('clear')  # Clear command for Unix/Linux/MacOS
